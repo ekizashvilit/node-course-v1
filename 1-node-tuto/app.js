335ded9cace85bcda91d765bf23a0427797144e5
ekizashvilit <tekizashvili@gmail.com>
2023-07-11 19:11:30 +0400

    async patterns - blocking

--- 1-node-tuto/app.js
+++ 1-node-tuto/app.js
@@ -1,3 +1,17 @@
-const os = require("os");
+const http = require('http');
 
-console.log(`system uptime is ${os.uptime()}`);
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('home page');
+  }
+
+  if (req.url === '/about') {
+    res.end('about page');
+  }
+
+  res.end('error page');
+});
+
+server.listen(5000, () => {
+  console.log('server is listening on port 5000...');
+});
