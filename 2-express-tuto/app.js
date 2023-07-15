d3e070a15429c67e7ac697f4887b5289559fd0d2
ekizashvilit <tekizashvili@gmail.com>
2023-07-15 16:44:15 +0400

    express basics

--- 2-express-tuto/app.js
+++ 2-express-tuto/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log(req.url);
+  res.status(200).send('home page');
+});
+
+app.get('/about', (req, res) => {
+  console.log(req.url);
+  res.status(200).send('about page');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send('not found');
+});
+
+app.listen(5000, () => {
+  console.log('server is running on port 5000...');
+});
