d6cb8135fbca77940ecf4b3cfbabe79487c0972f
ekizashvilit <tekizashvili@gmail.com>
2023-07-15 17:27:25 +0400

    express app example

--- 2-express-tuto/app.js
+++ 2-express-tuto/app.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.static('navbar-app'));
 
 app.get('/', (req, res) => {
-  console.log(req.url);
-  res.status(200).send('home page');
-});
-
-app.get('/about', (req, res) => {
-  console.log(req.url);
-  res.status(200).send('about page');
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
 });
 
 app.all('*', (req, res) => {
-  res.status(404).send('not found');
+  res.status(404).send('resource not found');
 });
 
 app.listen(5000, () => {
-  console.log('server is running on port 5000...');
+  console.log('server is listening on port 5000...');
 });
