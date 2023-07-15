5bfa47b9c548af57d7ef81e70cc7c7b2becd706b
ekizashvilit <tekizashvili@gmail.com>
2023-07-15 18:23:56 +0400

    express all static

--- 2-express-tuto/app.js
+++ 2-express-tuto/app.js
@@ -4,9 +4,9 @@ const path = require('path');
 
 app.use(express.static('navbar-app'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+// });
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found');
