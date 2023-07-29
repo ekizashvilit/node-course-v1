7bec7cc33cb3f1604af9292f377203e3f58ea0cc
ekizashvilit <tekizashvili@gmail.com>
2023-07-29 17:54:32 +0400

    all routes

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const tasks = require('./routes/tasks');
+
+app.use('/api/v1/tasks', tasks);
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
   res.send('task manager app');
 });
