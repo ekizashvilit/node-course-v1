df5fefaf82679625f537d66a7346f1144be7a553
ekizashvilit <tekizashvili@gmail.com>
2023-07-31 09:53:29 +0400

    connected to db

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
+const connectDB = require('./db/connect');
 const tasks = require('./routes/tasks');
 
-app.use('/api/v1/tasks', tasks);
 app.use(express.json());
+
+app.use('/api/v1/tasks', tasks);
 
 app.get('/hello', (req, res) => {
   res.send('task manager app');
@@ -12,4 +15,13 @@ app.get('/hello', (req, res) => {
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
