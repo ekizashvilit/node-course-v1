93a341a33bb1dedefba9fe6ebfe629149f31fda8
ekizashvilit <tekizashvili@gmail.com>
2023-08-02 12:36:54 +0400

    numeric filters

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -35,6 +35,32 @@ const getAllProducts = async (req, res) => {
   if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
+  }
+
+  // numeric filters
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
 
   const page = Number(req.query.page) || 1;
