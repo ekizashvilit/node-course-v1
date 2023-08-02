bb4addff1de754ee2711a38e7f9a6bbaf46857be
ekizashvilit <tekizashvili@gmail.com>
2023-08-02 10:05:58 +0400

    handling non-existent queries

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -1,10 +1,20 @@
-const getAllProductsStatic = (req, res) => {
-  throw new Error('testing errs');
-  res.status(200).json({ msg: 'testing route' });
+const Products = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Products.find({ featured: true });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  const products = await Products.find(queryObject);
+
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
