08279746005f7c476334761f95fb212d896a4c75
ekizashvilit <tekizashvili@gmail.com>
2023-08-07 16:37:53 +0400

    more error classes

--- 5-JWT-basics/controllers/main.js
+++ 5-JWT-basics/controllers/main.js
@@ -18,28 +18,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  const luckyNumber = Math.floor(Math.random() * 100);
 
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('no token provided', 401);
-  }
-
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    const luckyNumber = Math.floor(Math.random() * 100);
-
-    res
-      .status(200)
-      .json({
-        msg: `hello, ${decoded.username}`,
-        secret: `lucky number is ${luckyNumber}`,
-      });
-  } catch (error) {
-    throw new CustomAPIError('not authorized to access this route', 401);
-  }
+  res.status(200).json({
+    msg: `hello, ${req.user.username}`,
+    secret: `lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
