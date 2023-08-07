533f2b5c1da209316b02fed468d100bec7ad1103
ekizashvilit <tekizashvili@gmail.com>
2023-08-07 10:56:52 +0400

    sign jwt

--- 5-JWT-basics/controllers/main.js
+++ 5-JWT-basics/controllers/main.js
@@ -1,4 +1,5 @@
 const CustomAPIError = require('../errors/custom-error');
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -7,7 +8,13 @@ const login = async (req, res) => {
     throw new CustomAPIError('please provide email and password', 400);
   }
 
-  res.send('fake route');
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
