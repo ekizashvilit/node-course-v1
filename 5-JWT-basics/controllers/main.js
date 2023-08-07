8210523d10d0fdbe4d428149cd0dee114a8195e3
ekizashvilit <tekizashvili@gmail.com>
2023-08-07 16:18:21 +0400

    auth middleware setup

--- 5-JWT-basics/controllers/main.js
+++ 5-JWT-basics/controllers/main.js
@@ -18,10 +18,28 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({ msg: 'hello', secret: `lucky number is ${luckyNumber}` });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('no token provided', 401);
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res
+      .status(200)
+      .json({
+        msg: `hello, ${decoded.username}`,
+        secret: `lucky number is ${luckyNumber}`,
+      });
+  } catch (error) {
+    throw new CustomAPIError('not authorized to access this route', 401);
+  }
 };
 
 module.exports = {
