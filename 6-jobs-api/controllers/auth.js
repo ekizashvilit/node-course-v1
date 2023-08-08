8729cc3ecf09dac86cc1199caac3fd178edf2680
ekizashvilit <tekizashvili@gmail.com>
2023-08-08 17:52:40 +0400

    logic controller setup

--- 6-jobs-api/controllers/auth.js
+++ 6-jobs-api/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -9,7 +10,21 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError('please provide email and password');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new UnauthenticatedError('invalid credentials ');
+  }
+
+  const token = user.createJWT();
+
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
