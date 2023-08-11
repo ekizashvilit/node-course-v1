716f1a4ad8ebc2865465b002222ffc836d304f9f
ekizashvilit <tekizashvili@gmail.com>
2023-08-11 14:58:47 +0400

    validation error

--- 6-jobs-api/middleware/error-handler.js
+++ 6-jobs-api/middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -7,8 +6,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'something went wrong, try again later',
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+
+    customError.statusCode = 400;
   }
 
   if (err.code && err.code === 11000) {
