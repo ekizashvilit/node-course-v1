4cb3fd868bc90916658c27758c3e8603b7a27261
ekizashvilit <tekizashvili@gmail.com>
2023-08-13 10:39:24 +0400

    spring cleaning

--- 6.5-jobster-api/app.js
+++ 6.5-jobster-api/app.js
@@ -3,14 +3,7 @@ require('express-async-errors');
 
 // security packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
-const rateLimiter = require('express-rate-limit');
-
-// swagger
-const swaggerUI = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
 
 const express = require('express');
 const app = express();
@@ -25,23 +18,9 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.set('trust proxy', 1);
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-  })
-);
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
-
-app.get('/', (req, res) => {
-  res.send('<h1>jobs api</h1><a href="/api-docs">documentation</a>');
-});
-
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
