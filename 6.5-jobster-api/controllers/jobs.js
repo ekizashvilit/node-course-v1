67414f83d3c3a832ff360f174c33ca15fb561476
ekizashvilit <tekizashvili@gmail.com>
2023-08-14 09:49:37 +0400

    pagination

--- 6.5-jobster-api/controllers/jobs.js
+++ 6.5-jobster-api/controllers/jobs.js
@@ -39,9 +39,17 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position');
   }
 
-  const jobs = await result;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-  res.status(StatusCodes.OK).json({ jobs });
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result;
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
