49f6e2c609f6730673ce2461dd86705167e55a60
ekizashvilit <tekizashvili@gmail.com>
2023-08-22 09:19:48 +0400

    file/image upload starter files

--- 6.5-jobster-api/controllers/jobs.js
+++ 6.5-jobster-api/controllers/jobs.js
@@ -148,20 +148,20 @@ const showStats = async (req, res) => {
     { $limit: 6 },
   ]);
 
-  monthlyApplications = monthlyApplications.map((item) => {
-    const {
-      _id: { year, month },
-      count,
-    } = item;
-    const date = moment()
-      .month(month - 1)
-      .year(year)
-      .format('MMM Y');
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y');
 
-    return { date, count };
-  });
-
-  console.log(monthlyApplications);
+      return { date, count };
+    })
+    .reverse();
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
