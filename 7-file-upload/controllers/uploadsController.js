b8b61794c054a013857df42a35f5d8872c281f26
ekizashvilit <tekizashvili@gmail.com>
2023-08-22 13:01:14 +0400

    additional checks

--- 7-file-upload/controllers/uploadsController.js
+++ 7-file-upload/controllers/uploadsController.js
@@ -1,8 +1,24 @@
 const path = require('path');
 const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
 const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded');
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image');
+  }
+
+  const maxSize = 1024 * 1024;
+
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError('Please upload image smaller 1MB');
+  }
+
   const imagePath = path.join(
     __dirname,
     '../public/uploads/' + `${productImage.name}`
@@ -10,7 +26,7 @@ const uploadProductImage = async (req, res) => {
 
   await productImage.mv(imagePath);
 
-  res
+  return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
 };
