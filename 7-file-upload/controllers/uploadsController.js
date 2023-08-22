e49cd4e9dbc7305dd78ed39af261d8a7ab0c9552
ekizashvilit <tekizashvili@gmail.com>
2023-08-22 11:55:39 +0400

    express file upload

--- 7-file-upload/controllers/uploadsController.js
+++ 7-file-upload/controllers/uploadsController.js
@@ -1,7 +1,18 @@
+const path = require('path');
 const { StatusCodes } = require('http-status-codes');
 
 const uploadProductImage = async (req, res) => {
-  res.send('upload product image');
+  let productImage = req.files.image;
+  const imagePath = path.join(
+    __dirname,
+    '../public/uploads/' + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadProductImage };
